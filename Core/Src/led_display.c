8ffa15d7deb64c930b8b57e1b777175795f86159
jj-guidry <jadenjguidry@gmail.com>
2023-11-10 15:00:12 -0500

    trying to sync github

--- Core/Src/led_display.c
+++ Core/Src/led_display.c
@@ -29,16 +29,18 @@ void init_display(){
 	GPIOA->MODER |= GPIO_MODER_MODER5_1; // pa5 as AF
 	GPIOA->MODER |= GPIO_MODER_MODER15_1; // pa15 as AF
 
-	GPIOA->AFR[0] &= ~(0xf << 28); // pa7 as AF0 -> CLK
-	GPIOA->AFR[0] &= ~(0xf << 20); // pa5 as AF0  -> MOSI
+	GPIOA->AFR[0] &= ~(0xf << 28); // pa7 as AF0 -> MOSI
+	GPIOA->AFR[0] &= ~(0xf << 20); // pa5 as AF0  -> CLK
 	GPIOA->AFR[1] &= ~(0xf << 28); // pa15 as AF0 -> nSS
 
 	RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
 	SPI1->CR1 &= ~SPI_CR1_SPE; // turn off channel before config
 	while(SPI1->CR1 & SPI_CR1_SPE); // wait for it to be off
 	SPI1->CR1 |= SPI_CR1_BR; // set baud rate to all 1's
-	SPI1->CR2 |= SPI_CR2_DS_3 | SPI_CR2_DS_0; // 1001 DS = 10 bit word
+	SPI1->CR2 |= SPI_CR2_DS_3; // setting the DS is really wierd, see documentation, will be 0xf after this line
+	SPI1->CR2 &= ~(SPI_CR2_DS_2 | SPI_CR2_DS_1); // clear middle 2 bits so = 0b1001
 	SPI1->CR2 |= SPI_CR2_SSOE;
+	SPI1->CR1 |= SPI_CR1_MSTR;
 	SPI1->CR2 |= SPI_CR2_NSSP;
 	SPI1->CR1 |= SPI_CR1_SPE;
 
@@ -46,12 +48,11 @@ void init_display(){
 
 
 void led_send_command(uint16_t cmd){
-	while(!(SPI1->SR & SPI_SR_TXE)); // wait for there to be a transmission
+	while(!(SPI1->SR & SPI_SR_TXE)); // wait tx buffer empty
 	SPI1->DR = cmd;
 }
 
 
 void led_send_char(char ch){
-	led_send_command(0x02);
 	led_send_command(0x200 | ch);
 }
